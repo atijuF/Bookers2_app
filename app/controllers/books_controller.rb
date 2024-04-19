ae28ce4d039f5d5549c54dcc8f5dda8b587a284b
atijuF <162565598+atijuF@users.noreply.github.com>
2024-04-19 08:34:47 +0000

    [Add]Usersコントローラ作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error"
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @books = [Book.find(params[:id])]
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+  
+end
